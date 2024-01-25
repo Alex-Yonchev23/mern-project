e6b41f24a43a2e5ee1dd520258431ccf3e6bbd0c
Alex-Yonchev23 <125677851+Alex-Yonchev23@users.noreply.github.com>
2024-01-25 21:58:08 +0200

    create sign-up api route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+
+
 dotenv.config();
 
 
@@ -20,3 +23,4 @@ app.listen(5173, () => {
 });
 
 app.use("/server/user" , userRoutes);
+app.use("/server/auth" , authRoutes);
